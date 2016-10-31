a05d1631aa1fb8879a961d0412fe788b8736aa12
Narendra Choudhary <n.choudhary@iitg.ac.in>
2016-10-31 20:01:22 +0530

    Add AJAX form submission at fac-update

---
+++ iitgurp/static/js/faculty_update_ajax.js
@@ -0,0 +1,18 @@
+function update_profile() {
+    $.ajax({
+        url: "",
+        type: "POST",
+        data: {
+            web_mail: $('#id_web_mail').val(),
+            department: $('#id_department').val(),
+            room_no: $('#id_room_no').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+        },
+        success: function (json) {
+            alert("profile updatde successfully");
+        },
+        error : function(xhr,errmsg,err){
+            alert("some error occured while updating profile");
+        }
+    });
+}
